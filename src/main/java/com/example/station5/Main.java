ba0f8bcb6259423e9371ceafeda6d1279e643167
ShoFukuyama <sho.fukuyama0123@gmail.com>
2024-12-04 16:15:22 +0900

    1

--- src/main/java/com/example/station5/Main.java
+++ src/main/java/com/example/station5/Main.java
@@ -3,6 +3,16 @@ package com.example.station5;
 public class Main {
     public static void main(String[] args) {
         // ここから
+        @SuppressWarnings("unused")
+        int userA = 23;
+        int userB = 36;
+        int userC = 31;
+        int userD = 48;
+        int userE = 58;
+
+        double all = userA + userB + userC + userD + userE;
+
+        System.out.println(all / 5);
         // ここまで
     }
 }
