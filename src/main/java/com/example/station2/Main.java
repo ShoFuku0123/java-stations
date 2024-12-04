b88daa512ecb7e00bda7d0ed2aa4b2582a54222a
ShoFukuyama <sho.fukuyama0123@gmail.com>
2024-12-04 09:39:44 +0900

    station2

--- src/main/java/com/example/station2/Main.java
+++ src/main/java/com/example/station2/Main.java
@@ -3,6 +3,14 @@ package com.example.station2;
 public class Main {
     public static void main(String[] args) {
         // ここから
+        boolean a = false;
+        System.out.println(a);
+        
+        Integer i = 42;
+        System.out.println(i);
+
+        double d = 3.141592;
+        System.out.println(d);
         // ここまで
     }
 }
